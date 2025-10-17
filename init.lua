a7ece5d0be479cee40faade40555e22c8c5bcd1c
Pandya S. <pandyasakaaji@gmail.com>
2025-10-17 15:06:24 +0700

    Removing background color

--- init.lua
+++ init.lua
@@ -1012,5 +1012,12 @@ require('lazy').setup({
   },
 })
 
+-- Hapus background default
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
